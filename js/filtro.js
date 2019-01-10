2f5deb02df9e4fd34f5b78c957bbb1e734ae2491
jpnr2016 <jpnr2016@gmail.com>
2019-01-10 08:35:23 -0200

    Aula 08 concluida

--- js/filtro.js
+++ js/filtro.js
@@ -1,13 +1,28 @@
-CONTINUAR AULA 08 03
+AULA 09 01 AJAX
 
 var campoFiltro = document.querySelector("#filtrar-tabela");
 
-campoFiltro.addEventListener("input", function(){
-    console.log("digitaram no campo");
-    var pacientes = document.querySelectorAll(".paciente");
-    pacientes.forEach(function(paciente) {
-        var paciente = pacientes;
-        var tdNome = paciente.querySelector(".info-nome");
-        var nome = paciente.textContent;
-    });
+campoFiltro.addEventListener("input", function() {
+  var pacientes = document.querySelectorAll(".paciente");
+
+  if (this.value.length > 0) {
+    for (var i = 0; i < pacientes.length; i++) {
+      var paciente = pacientes[i];
+      var tdNome = paciente.querySelector(".info-nome");
+      var nome = tdNome.textContent;
+      var expressao = new RegExp(this.value, "i"); // o "i" se a busca é caseSensitive ou Insensitive(aceita maiuscula ou minusculo na busca)
+      if (!expressao.test(nome)) { //estou testando se no nome tem um pedacinho do this.value da expressão regular
+        paciente.classList.add("invisivel");
+      } else {
+        paciente.classList.remove("invisivel");
+      }
+    }
+  }else{
+      for(var i = 0; i < pacientes.length; i++){
+          var paciente = pacientes[i];
+          paciente.classList.remove("invisivel");
+      }
+  }
 });
+
+
